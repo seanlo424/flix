5c9104aec1d549f6a48b678d7af2a20bac3d6115
Sean O'Neil <sean.oneil424@gmail.comgit config --global user.name Sean>
2019-08-28 22:15:48 +0000

    Flixter Update

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   #devise_for :models
 	root 'static_pages#index'
 	resources :courses, only: [:index, :show]
+	resources :lessons, only: [:show]
 	namespace :instructor do
 		resources :sections, only: [] do
 			resources :lessons, only: [:new, :create]
