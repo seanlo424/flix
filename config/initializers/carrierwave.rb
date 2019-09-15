bb0cd6ac993c2960931ae8162fdb9b7705adfc04
Sean O'Neil <sean.oneil424@gmail.comgit config --global user.name Sean>
2019-09-15 18:46:14 +0000

    Debugging

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
 	config.aws_bucket = ENV["AWS_BUCKET"]
 	config.aws_acl = "public-read"
 	
-	config.fog_credentials = {
+	config.aws_credentials = {
 		aws_access_key_id: ENV["AWS_ACCESS_KEY"], aws_secret_access_key: ENV["AWS_SECRET_KEY"], region: ENV["AWS_REGION"]
 	}
 end
